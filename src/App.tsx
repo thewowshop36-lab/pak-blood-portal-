0f64de6a71cd64b3112499ea7f33ceb2931b052d
thewowshop36-lab <thewowshop36@gmail.com>
2026-09-22 22:35:57 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,8 @@ export default function App() {
 
   const filteredCurrentCityDonors = useMemo(() => {
     return donors.filter((d) => {
-      const matchCity = (d.city || '').toLowerCase().includes((selectedCity || '').toLowerCase().split('/')[0].trim());
+      const matchCity = true;
+
 
       const matchBlood = selectedBlood ? d.blood_group === selectedBlood : true;
       let matchSearch = true;
