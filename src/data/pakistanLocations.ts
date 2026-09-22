3431fc10401c82173566fcfac7cb27d310e80250
thewowshop36-lab <thewowshop36@gmail.com>
2026-09-22 16:29:35 +0400

    Create pakistanLocations.ts

---
+++ src/data/pakistanLocations.ts
@@ -0,0 +1,236 @@
+export const pakistanProvinces: Record<string, string[]> = {
+  "پنجاب / Punjab": [
+    "خانیوال / Khanewal",
+    "لاہور / Lahore",
+    "ملتان / Multan",
+    "فیصل آباد / Faisalabad",
+    "راولپنڈی / Rawalpindi",
+    "گوجرانوالہ / Gujranwala",
+    "سیالکوٹ / Sialkot",
+    "بہاولپور / Bahawalpur",
+    "ساہیوال / Sahiwal",
+    "سرگودھا / Sargodha",
+    "وہاڑی / Vehari",
+    "شیخوپورہ / Sheikhupura",
+    "گجرات / Gujrat",
+    "قصور / Kasur",
+    "رحیم یار خان / Rahim Yar Khan",
+    "اوکاڑہ / Okara",
+    "ڈی جی خان / D.G. Khan",
+    "مظفر گڑھ / Muzaffargarh",
+    "منڈی بہاؤالدین / Mandi Bahauddin",
+    "ٹوبہ ٹیک سنگھ / Toba Tek Singh",
+    "جھنگ / Jhang",
+    "خوشاب / Khushab",
+    "میانوالی / Mianwali",
+    "لودھراں / Lodhran",
+    "بھکر / Bhakkar",
+    "بہاولنگر / Bahawalnagar",
+    "پاکپتن / Pakpattan",
+    "چکوال / Chakwal",
+    "اٹک / Attock",
+    "جہلم / Jhelum",
+    "چینیوٹ / Chiniot",
+    "حافظ آباد / Hafizabad",
+    "نارووال / Narowal",
+    "راجن پور / Rajanpur",
+    "احمد پور شرقیہ / Ahmedpur East"
+  ],
+  "سندھ / Sindh": [
+    "کراچی / Karachi",
+    "حیدرآباد / Hyderabad",
+    "سکھر / Sukkur",
+    "لاڑکانہ / Larkana",
+    "نوابشاہ / Nawabshah",
+    "میرپور خاص / Mirpur Khas",
+    "بدین / Badin",
+    "گھوٹکی / Ghotki",
+    "جیکب آباد / Jacobabad",
+    "جامشورو / Jamshoro",
+    "کشمور / Kashmore",
+    "مٹیاری / Matiari",
+    "نوشہروفیروز / Naushahro Feroze",
+    "شکارپور / Shikarpur",
+    "ٹنڈو الہ یار / Tando Allahyar",
+    "ٹنڈو محمد خان / Tando Muhammad Khan",
+    "تھرپارکر / Tharparkar",
+    "عمرکوٹ / Umerkot"
+  ],
+  "خیبر پختونخوا / KPK": [
+    "پشاور / Peshawar",
+    "مردان / Mardan",
+    "ایبٹ آباد / Abbottabad",
+    "سوات / Swat",
+    "ڈیرہ اسماعیل خان / D.I. Khan",
+    "کوہاٹ / Kohat",
+    "بنوں / Bannu",
+    "ہری پور / Haripur",
+    "صوابی / Swabi",
+    "چارسدہ / Charsadda",
+    "نوشہرہ / Nowshera",
+    "چترال / Chitral",
+    "ٹانک / Tank"
+  ],
+  "بلوچستان / Balochistan": [
+    "کوئٹہ / Quetta",
+    "گوادر / Gwadar",
+    "خضدار / Khuzdar",
+    "لسبیلہ / Lasbela",
+    "مستونگ / Mastung",
+    "سبی / Sibi",
+    "ژوب / Zhob"
+  ],
+  "اسلام آباد / Islamabad": [
+    "اسلام آباد / Islamabad Capital Territory"
+  ],
+  "آزاد کشمیر و گلگت / AJK & GB": [
+    "مظفر آباد / Muzaffarabad",
+    "میرپور / Mirpur",
+    "راولاکوٹ / Rawalakot",
+    "گلگت / Gilgit",
+    "سکردو / Skardu",
+    "ہنزہ / Hunza"
+  ]
+};
+
+export const cityTehsils: Record<string, string[]> = {
+  "خانیوال / Khanewal": [
+    "خانیوال سٹی / Khanewal City",
+    "کبیروالا / Kabirwala",
+    "میاں چنوں / Mian Channu",
+    "جہانیاں / Jahanian",
+    "عبدالحکیم / Abdul Hakeem"
+  ],
+  "ملتان / Multan": [
+    "ملتان سٹی / Multan City",
+    "ملتان کینٹ / Multan Cantt",
+    "شجاع آباد / Shujabad",
+    "جلال پور پیروالا / Jalalpur Pirwala"
+  ],
+  "لاہور / Lahore": [
+    "گلبرگ / Gulberg",
+    "ماڈل ٹاؤن / Model Town",
+    "ڈی ایچ اے / DHA",
+    "جوہر ٹاؤن / Johar Town",
+    "اقبال ٹاؤن / Allama Iqbal Town",
+    "شاہدرہ / Shahdara",
+    "کینٹ / Cantt"
+  ],
+  "فیصل آباد / Faisalabad": [
+    "فیصل آباد سٹی / Faisalabad City",
+    "جڑانوالہ / Jaranwala",
+    "سمندری / Samundri",
+    "تاندرلیانوالہ / Tandlianwala"
+  ],
+  "راولپنڈی / Rawalpindi": [
+    "راولپنڈی کینٹ / Cantt",
+    "صدر / Saddar",
+    "سیٹلائٹ ٹاؤن / Satellite Town",
+    "گوجر خان / Gujar Khan",
+    "ٹیکسلا / Taxila"
+  ],
+  "کراچی / Karachi": [
+    "صدر / Saddar",
+    "گلشن اقبال / Gulshan-e-Iqbal",
+    "کورنگی / Korangi",
+    "کلفٹن / Clifton",
+    "نارتھ ناظم آباد / North Nazimabad",
+    "ملیر / Malir"
+  ]
+};
+
+export const nearbyCities: Record<string, string[]> = {
+  "خانیوال / Khanewal": [
+    "ملتان / Multan",
+    "وہاڑی / Vehari",
+    "ساہیوال / Sahiwal",
+    "ٹوبہ ٹیک سنگھ / Toba Tek Singh",
+    "لودھراں / Lodhran",
+    "جھنگ / Jhang"
+  ],
+  "ملتان / Multan": [
+    "خانیوال / Khanewal",
+    "مظفر گڑھ / Muzaffargarh",
+    "لودھراں / Lodhran",
+    "بہاولپور / Bahawalpur",
+    "وہاڑی / Vehari"
+  ],
+  "لاہور / Lahore": [
+    "شیخوپورہ / Sheikhupura",
+    "قصور / Kasur",
+    "گوجرانوالہ / Gujranwala",
+    "اوکاڑہ / Okara"
+  ],
+  "فیصل آباد / Faisalabad": [
+    "چینیوٹ / Chiniot",
+    "ٹوبہ ٹیک سنگھ / Toba Tek Singh",
+    "جھنگ / Jhang",
+    "شیخوپورہ / Sheikhupura"
+  ],
+  "ساہیوال / Sahiwal": [
+    "اوکاڑہ / Okara",
+    "پاکپتن / Pakpattan",
+    "خانیوال / Khanewal",
+    "وہاڑی / Vehari"
+  ],
+  "وہاڑی / Vehari": [
+    "خانیوال / Khanewal",
+    "لودھراں / Lodhran",
+    "بہاولنگر / Bahawalnagar",
+    "پاکپتن / Pakpattan"
+  ],
+  "راولپنڈی / Rawalpindi": [
+    "اسلام آباد / Islamabad Capital Territory",
+    "اٹک / Attock",
+    "چکوال / Chakwal",
+    "جہلم / Jhelum"
+  ],
+  "اسلام آباد / Islamabad Capital Territory": [
+    "راولپنڈی / Rawalpindi",
+    "ہری پور / Haripur",
+    "ایبٹ آباد / Abbottabad"
+  ]
+};
+
+export const bloodCompatibility: Record<string, { canGiveTo: string[]; canReceiveFrom: string[]; tagUrdu: string }> = {
+  "O-": {
+    canGiveTo: ["O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+"],
+    canReceiveFrom: ["O-"],
+    tagUrdu: "یونیورسل ڈونر (سب کو خون دے سکتا ہے)"
+  },
+  "O+": {
+    canGiveTo: ["O+", "A+", "B+", "AB+"],
+    canReceiveFrom: ["O+", "O-"],
+    tagUrdu: "سب سے زیادہ مطلوب بلڈ گروپ"
+  },
+  "A-": {
+    canGiveTo: ["A-", "A+", "AB-", "AB+"],
+    canReceiveFrom: ["A-", "O-"],
+    tagUrdu: "نایاب بلڈ گروپ"
+  },
+  "A+": {
+    canGiveTo: ["A+", "AB+"],
+    canReceiveFrom: ["A+", "A-", "O+", "O-"],
+    tagUrdu: "عام دستیاب بلڈ گروپ"
+  },
+  "B-": {
+    canGiveTo: ["B-", "B+", "AB-", "AB+"],
+    canReceiveFrom: ["B-", "O-"],
+    tagUrdu: "نایاب بلڈ گروپ"
+  },
+  "B+": {
+    canGiveTo: ["B+", "AB+"],
+    canReceiveFrom: ["B+", "B-", "O+", "O-"],
+    tagUrdu: "پاکستان میں سب سے زیادہ پایا جانے والا گروپ"
+  },
+  "AB-": {
+    canGiveTo: ["AB-", "AB+"],
+    canReceiveFrom: ["AB-", "A-", "B-", "O-"],
+    tagUrdu: "انتہائی نایاب بلڈ گروپ"
+  },
+  "AB+": {
+    canGiveTo: ["AB+"],
+    canReceiveFrom: ["O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+"],
+    tagUrdu: "یونیورسل وصول کنندہ (ہر کسی سے خون لے سکتا ہے)"
+  }
+};
